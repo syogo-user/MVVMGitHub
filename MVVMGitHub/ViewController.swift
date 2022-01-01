ce2a57eaa2445ef2faca6b12d7919a67f10b97db
syogo-user <s.photo.trumpet.005@gmail.com>
2022-01-01 12:16:15 +0900

    Initial Commit

---
+++ MVVMGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMGitHub
+//
+//  Created by 小野寺祥吾 on 2022/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
